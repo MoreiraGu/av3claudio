74fad83a6a37130ba466a580482315c96c6b7f52
Gustavo Moreira <santosmoreiragustavo7@gmail.com>
2025-11-14 11:17:24 -0300

    Modify database connection credentials
    Updated database connection credentials to be empty for user and password.

--- db.js
+++ db.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: 'fatec',  // coloque sua senha
+  user: '', // coloque seu usuário
+  password: '',  // coloque sua senha
   database: 'portfolio',
   waitForConnections: true,
   connectionLimit: 10,
